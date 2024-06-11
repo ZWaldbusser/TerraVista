b38535d381c934796b3c263f28b2cb39ab6fd2ae
ZWaldbusser <zwaldbusser@gmail.com>
2024-06-11 15:19:14 -0500

    Beginning the transition to .csv datasets
    Still not sure if I want the loading to happen in Dataset.h or FileLoader.h

--- Dataset.h
+++ Dataset.h
@@ -1,10 +1,15 @@
 #ifndef DATASET_H
 #define DATASET_H
 
-//Preliminary implementation of a class to hold datasets.
 //Potential problems: dynamically creating new variables, keeping track of inputs/creating t[hem from file I/O?
 
-
+/*
+    Notes: 6/11/2024
+    Code has effectively been obliterated.
+    Won't work until i get either the .csv importer OR the default constructor to match our old housing problem.
+    Maybe use vector of vectors for each datapoint?
+    Will need to use memory management VERY effectively
+*/
 
 
 class Dataset{
@@ -14,10 +19,10 @@ class Dataset{
 
     //Base implementation will be based off of the previous code. Will need to configure a dataset and how to take input.
     //Gradient step in main?
-    int numEntries = 5;
-    int dimensions = 3;
-    int trainingVal[3][5] = {{1, 1, 1, 1, 1}, {2104, 1600, 2400, 1416, 3000}, {3, 3, 3, 2, 4}};
-    int price[5] = {400, 330, 369, 232, 540};
+    int numEntries;
+    int dimensions;
+    int** trainingVal;
+    int* result;
 
     //Weights are the only thing that need to change
     float* weights;
@@ -28,6 +33,7 @@ class Dataset{
     //Rule of 3
     Dataset();
     //Try doing a constructor with a new object that takes in an xml file.
+    Dataset(string fileName);
     ~Dataset();
 
     float weightedVals(int x);
@@ -45,13 +51,26 @@ class Dataset{
 Dataset::Dataset(){
     //Need to update once I take in new datasets
     weights = new float[3]();
+}
 
+Dataset::Dataset(string fileName){
+    //Try making input here
+    //Assume given value is the last value of every datapoint.
 
+    //1. Create istream, begin reading the file.
 
+    //2. Use the first line to determine dimensions
 
+    //3. Iterate through each line, pulling the float values from each line
+
+    //3.5. Make sure to DYNAMICALLY do everything. account for ;/, seperators. 
+
+    //4. Make sure to close the file 
 
 
 }
+
+
 Dataset::~Dataset() {
     //Need to update
 }
--- FileLoader.h
+++ FileLoader.h
@@ -6,19 +6,46 @@ class FileLoader{
     //Blueprinting Idea: An object that loads in xml files for datasets
 
     /*
-    A potential cause for the current Issue is too small of datasets
-    Too little points could cause too variable of a change
-    Would be worth looking into larger datasets with predetermined steps/outcomes
-    Today's work will be mainly research, aside from the creation of this object
+    Time to get this to work
+    Datasets were pulled from UC Irvines database
+    Both sets are 11 dimensions. The dynamic cpp can handle these new dimensions.
+    The redwine csv has 
     
     */
 
 
 
+
+
     public:
+    //Rule of 3 needs to be applied here
+    FileLoader();
+    FileLoader(string fileName);
+    FileLoader& operator=(const FileLoader& toCopy);
+    FileLoader();
+
 
 
 };
+
+FileLoader::FileLoader() {
+
+}
+
+FileLoader::FileLoader(string fileName) {
+    //To actually import the csv here.
+    //Potentially find a library or end up actually
+    //ACTUALLY EDIT THE DATASET
+
+
+    //Not even sure if i need this object. Might just be able to do a Dataset(".csv")
+
+
+}
+
+FileLoader& FileLoader::operator=(const FileLoader& toCopy){
+
+}
 
 
 
