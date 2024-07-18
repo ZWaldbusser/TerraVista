0ec99b9f9b6df197a0ba37400b7fa470e6c95797
ZWaldbusser <zwaldbusser@gmail.com>
2024-07-18 17:02:27 -0500

    Update SVM.cpp
    More research and learning about SVMs. Not sure if this is something replicatable in .cpp but I'll give it a shot.

--- SVM/SVM.cpp
+++ SVM/SVM.cpp
@@ -7,6 +7,26 @@ Currently: Beginning to learn about the algorithm. Unsure of implementation.
         We basically are trying to find the maximum hyperplane between sets, in order to "separate" the chunks of data/
 
 
+        Supporting terminology:
+
+        Threshold: The hyperplane at which a point changes classifications
+        Margin: the "space" between the closest point to the hyperplane and the threshold
+
+        In order to make our margin less sensitive, we must allow for misclassifications
+
+
+
+        Kernel function: a function used to take in data and stransform it to the form its needed in.
+            Used to find the Support Vector Classifier
+
+            Polynomial, Radial, and Exponential kernels functions are some examples.
+
+        The Kernel Trick: Treating the data as if it already exists in a higher dimension, and just comparing the data to itself.
+            This reduces computation power, by avoiding lots of transform math.
+            
+
+
+
 
 
 
